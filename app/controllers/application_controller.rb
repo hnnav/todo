184cbfa6d5ba7195f3309ed5d0856c1a42374326
hnnav <henna.v@icloud.com>
2020-06-16 15:17:41 +0300

    navbar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :current_user
+    # gives access in views
+    helper_method :current_user, :logged_in?
 
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
